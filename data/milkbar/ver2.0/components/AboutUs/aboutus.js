12547e8ef73287e6f3dabb2617954a0fd7910c28
Michał Matynia <mmatynia@gmail.com>
2023-04-06 00:46:54 +0200

    vdv

---
+++ data/milkbar/ver2.0/components/AboutUs/aboutus.js
@@ -0,0 +1,8 @@
+import bgImage from 'public/img/milkbar/ver2.0/parallax/ddla01korekta-nwsmall.jpg'
+export const aboutus = [
+  {
+    parallax: {
+      image: { src: bgImage.src },
+    },
+  },
+]
